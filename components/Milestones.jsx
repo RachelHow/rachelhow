2701de72bc9743af91c5448229e7f67e21e18ece
Rachel How <rachelhow5242@gmail.com>
2020-09-14 17:30:38 +0800

    Milestone updates

--- components/Milestones.jsx
+++ components/Milestones.jsx
@@ -17,6 +17,14 @@ const Milestones = () => (
       </div> */}
       <div className="flex pb-4 sm:pb-8 md:flex-col sm:flex-col">
         <p className="text-s md:text-xxs text-lightgray uppercase tracking-wider pr-12 leading-12">
+          Sep 14, 2020
+        </p>
+        <h4 className="text-base font-b">
+          Guest Speaker at Monash University Malaysia
+        </h4>
+      </div>
+      <div className="flex pb-4 sm:pb-8 md:flex-col sm:flex-col">
+        <p className="text-s md:text-xxs text-lightgray uppercase tracking-wider pr-12 leading-12">
           Aug 31, 2020
         </p>
         <h4 className="text-base font-b">
